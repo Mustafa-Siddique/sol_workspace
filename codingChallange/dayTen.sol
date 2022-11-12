d112391e7b519ee65ccfe17435a9155ed915aae9
Mustafa-Siddique <mstfasid@gmail.com>
2022-11-12 12:20:01 +0530

    dayTen

---
+++ codingChallange/dayTen.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+contract dayTen {
+
+    function power(uint x, uint y) public pure returns(uint){
+        return x**y;
+    }
+    
+}
