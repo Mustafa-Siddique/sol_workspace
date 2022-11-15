782ab36c61ccda9a4417a329b840e426592b9998
Mustafa-Siddique <mstfasid@gmail.com>
2022-11-16 00:40:33 +0530

    dayThirteen

---
+++ codingChallange/dayThirteen.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+contract dayThirteen{
+
+    function reverseArray(uint256[] memory argArray, uint256 arrLength) public pure returns(uint256[] memory){
+        uint256 temp;
+        
+        for(uint256 index = 0; index < arrLength/2; index++){
+            temp = argArray[index];
+            argArray[index] = argArray[arrLength - index - 1];
+            argArray[arrLength - index - 1] = temp;
+        }
+
+        return argArray;
+    }
+    
+}
