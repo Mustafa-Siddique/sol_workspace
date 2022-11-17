41772061c8995f0826c4bbe882723a0d486def0c
Mustafa-Siddique <mstfasid@gmail.com>
2022-11-17 23:08:02 +0530

    dayFifteenInitialised

---
+++ codingChallange/dayFifteen.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+contract dayFifteen {
+    
+    function sort(uint256[] memory array, uint256 length) public pure returns(uint256[] memory) {
+        uint256 temp;
+
+        for (uint8 i = 0; i < length; i++) 
+        {
+            temp = array[i];
+        }
+
+        return array;
+    }
+}
