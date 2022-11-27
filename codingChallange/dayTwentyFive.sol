0548385f2d8849d79de7a519733634989cb4da26
Mustafa-Siddique <mstfasid@gmail.com>
2022-11-27 22:37:41 +0530

    dayTwentyFive

---
+++ codingChallange/dayTwentyFive.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+contract dayTwentyFive {
+
+    enum House {SMALL, MEDIUM, LARGE}
+
+    House _variable;
+
+    function setLarge() public {
+        _variable = House.LARGE; // setting value to declared variable
+    }
+
+    function getChoice() public view returns(House){
+        return _variable; // returning variable's value
+    }
+
+}
