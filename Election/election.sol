040ccf1d33de5c37f6e78686b2ea499c22610003
Mustafa-Siddique <mstfasid@gmail.com>
2023-03-06 11:00:46 +0530

    formatting

--- Election/election.sol
+++ Election/election.sol
@@ -2,15 +2,19 @@
 
 pragma solidity ^0.8.15;
 
-contract election{
-
+contract election {
     address public owner;
     string electionName;
     string electionDesc;
     uint256 electionEndTime;
     bool electionEnded;
 
-    constructor(address _owner, string memory _title, string memory _description, uint256 time){
+    constructor(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 time
+    ) {
         owner = _owner;
         electionName = _title;
         electionDesc = _description;
@@ -18,7 +22,7 @@ contract election{
         electionEnded = false;
     }
 
-    struct politicalParty{
+    struct politicalParty {
         string partyName;
         string partySlogan;
         string logoUri;
@@ -27,13 +31,13 @@ contract election{
         string officeAddress;
     }
 
-    mapping(uint8=>politicalParty) public parties;
+    mapping(uint8 => politicalParty) public parties;
 
-    struct Voter{
+    struct Voter {
         string name;
         string ppsnNumber; // PPSN Number - String
         uint128 dob; // must be entered in epoch format visit this for conversion https://www.epochconverter.com
-        string residentialAddress; 
+        string residentialAddress;
         string photoUri;
         string email; // Email Verification
         address wallet;
@@ -41,46 +45,91 @@ contract election{
         bool voted;
     }
 
-    mapping(uint8=>Voter) voters;
+    mapping(uint8 => Voter) voters;
 
     uint8 numParties = 1;
     uint8 numVoters = 1;
 
-    function addParty(string memory _name, string memory _slogan, string memory _logoUri, string memory _mail, string memory _address) public {
-        require(owner == msg.sender, "You don't have the authority to create a Political Party.");
+    function addParty(
+        string memory _name,
+        string memory _slogan,
+        string memory _logoUri,
+        string memory _mail,
+        string memory _address
+    ) public {
+        require(
+            owner == msg.sender,
+            "You don't have the authority to create a Political Party."
+        );
 
         uint8 _id = numParties;
-        parties[_id] = politicalParty(_name, _slogan, _logoUri, 0, _mail, _address);
+        parties[_id] = politicalParty(
+            _name,
+            _slogan,
+            _logoUri,
+            0,
+            _mail,
+            _address
+        );
         numParties++;
     }
 
-    function getNumOfParties() public view returns(uint8){
+    function getNumOfParties() public view returns (uint8) {
         return (numParties - 1);
     }
 
-    function addVoter(string memory _name, string memory _ppsn, uint128 _dob, string memory _address, string memory photo, string memory email) public {
+    function addVoter(
+        string memory _name,
+        string memory _ppsn,
+        uint128 _dob,
+        string memory _address,
+        string memory photo,
+        string memory email
+    ) public {
         uint8 _id = numVoters;
-        voters[_id] = Voter(_name, _ppsn, _dob, _address, photo, email, msg.sender, false, false);
+        voters[_id] = Voter(
+            _name,
+            _ppsn,
+            _dob,
+            _address,
+            photo,
+            email,
+            msg.sender,
+            false,
+            false
+        );
         numVoters++;
     }
 
-    function getNumOfVoters() public view returns(uint8){
+    function getNumOfVoters() public view returns (uint8) {
         return (numVoters - 1);
     }
 
-    function getVoterDetails(uint8 id) public view returns(Voter memory){
-        require(msg.sender == owner || msg.sender == voters[id].wallet, "You don't have authority to access this information.");
+    function getVoterDetails(uint8 id) public view returns (Voter memory) {
+        require(
+            msg.sender == owner || msg.sender == voters[id].wallet,
+            "You don't have authority to access this information."
+        );
         return voters[id];
     }
 
-    function verifyVoter(uint8 id) public returns(string memory){
-        require(msg.sender == voters[id].wallet, "You can't verify this voter.");
+    function verifyVoter(uint8 id) public returns (string memory) {
+        require(
+            msg.sender == voters[id].wallet,
+            "You can't verify this voter."
+        );
         voters[id].verified = true;
         return ("You have been verified successfully!");
     }
 
-    function vote(uint8 _partyId, uint8 _voterId) public returns(string memory){
-        require(msg.sender == voters[_voterId].wallet, "You can't vote on others behalf.");
+    function vote(uint8 _partyId, uint8 _voterId)
+        public
+        returns (string memory)
+    {
+        require(
+            msg.sender == voters[_voterId].wallet,
+            "You can't vote on others behalf."
+        );
         require(!voters[_voterId].voted, "You cannot double vote.");
         require(block.timestamp <= electionEndTime, "Election ended.");
 
@@ -89,11 +138,11 @@ contract election{
         return ("You have voted successfully!");
     }
 
-    function winnerDetails() public view returns(politicalParty memory){
+    function winnerDetails() public view returns (politicalParty memory) {
         uint8 partyId;
-        for(uint8 i = 1; i < numParties; i++){
+        for (uint8 i = 1; i < numParties; i++) {
             uint16 tempVoteCount = 0;
-            if(tempVoteCount < parties[i].voteCount){
+            if (tempVoteCount < parties[i].voteCount) {
                 tempVoteCount = parties[i].voteCount;
                 partyId = i;
             }
@@ -101,7 +150,11 @@ contract election{
         return parties[partyId];
     }
 
-    function getElectionDetails() public view returns(string memory, string memory){
-        return(electionName,electionDesc);
+    function getElectionDetails()
+        public
+        view
+        returns (string memory, string memory)
+    {
+        return (electionName, electionDesc);
     }
 }
