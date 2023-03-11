ce96d9d4739cff1292c20c1ce17dd1be05be8528
Mustafa-Siddique <mstfasid@gmail.com>
2023-03-11 16:20:02 +0530

    formatting

--- Election/votingFactory.sol
+++ Election/votingFactory.sol
@@ -4,36 +4,51 @@ pragma solidity ^0.8.15;
 
 import "./election.sol";
 
-contract votingFactory{
-    
-    struct electionDetails{
+contract votingFactory {
+    struct electionDetails {
         address deployedAddress;
         string electionName;
         string electionDesc;
         address authorityAddress;
     }
 
-    mapping(uint8=>electionDetails) _electionDetails;
+    mapping(uint8 => electionDetails) _electionDetails;
 
     uint8 numElection = 1;
 
-    function createElection(string memory title, string memory description, uint256 endTime) public {
-        address electionAddress = address(new election(msg.sender, title, description, endTime));
+    function createElection(
+        string memory title,
+        string memory description,
+        uint256 endTime
+    ) public {
+        address electionAddress = address(
+            new election(msg.sender, title, description, endTime)
+        );
 
-        _electionDetails[numElection] = electionDetails(electionAddress, title, description, msg.sender);
+        _electionDetails[numElection] = electionDetails(
+            electionAddress,
+            title,
+            description,
+            msg.sender
+        );
         numElection++;
     }
 
-    function getElection(uint8 id) public view returns(electionDetails memory){
+    function getElection(uint8 id)
+        public
+        view
+        returns (electionDetails memory)
+    {
         return _electionDetails[id];
     }
 
     function getAllElections() public view returns (electionDetails[] memory) {
-        electionDetails[] memory elections = new electionDetails[](numElection - 1);
+        electionDetails[] memory elections = new electionDetails[](
+            numElection - 1
+        );
         for (uint8 i = 1; i < numElection; i++) {
             elections[i - 1] = _electionDetails[i];
         }
         return elections;
     }
-
 }
