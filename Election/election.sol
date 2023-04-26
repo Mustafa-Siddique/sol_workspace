fc649db646ad521124f0abdac589a5d8edd2feb7
Mustafa-Siddique <mstfasid@gmail.com>
2023-04-26 18:57:51 +0530

    election-voter-verification

--- Election/election.sol
+++ Election/election.sol
@@ -60,6 +60,9 @@ contract election {
 
     mapping(uint8 => Voter) voters;
 
+    // mapping to store voter verification uri for identity verification
+    mapping(address => string) voterVerificationUri;
+
     uint8 numParties = 1;
     uint8 numVoters = 1;
 
@@ -118,6 +121,40 @@ contract election {
         numVoters++;
     }
 
+    function uploadVerification(string memory assetURI, uint8 _voterId) public {
+        require(
+            msg.sender == voters[_voterId].wallet,
+            "You don't have authority to upload verification."
+        );
+        require(
+            keccak256(abi.encodePacked(voterVerificationUri[msg.sender])) ==
+                keccak256(abi.encodePacked("")) ||
+                keccak256(abi.encodePacked(voterVerificationUri[msg.sender])) ==
+                keccak256(abi.encodePacked(" ")),
+            "You have already uploaded verification."
+        );
+        voterVerificationUri[msg.sender] = assetURI;
+    }
+
+    function checkVerification(
+        address _voterAddress
+    ) public view returns (string memory) {
+        require(
+            msg.sender == owner || msg.sender == _voterAddress,
+            "You don't have authority to access this information."
+        );
+        if (
+            keccak256(abi.encodePacked(voterVerificationUri[_voterAddress])) ==
+            keccak256(abi.encodePacked("")) ||
+            keccak256(abi.encodePacked(voterVerificationUri[_voterAddress])) ==
+            keccak256(abi.encodePacked(" "))
+        ) {
+            return "Not Verified";
+        } else {
+            return voterVerificationUri[_voterAddress];
+        }
+    }
+
     function getNumOfVoters() public view returns (uint8) {
         return (numVoters - 1);
     }
@@ -132,41 +169,47 @@ contract election {
 
     function verifyVoter(uint8 id) public returns (string memory) {
         require(
-            msg.sender == voters[id].wallet,
-            "You can't verify this voter."
+            msg.sender == owner,
+            "You don't have authority to verify voters"
         );
+        require(!voters[id].verified, "Voter already verified.");
         voters[id].verified = true;
         return ("You have been verified successfully!");
     }
 
-    function vote(uint8 _partyId, uint8 _voterId)
-        public
-        returns (string memory)
-    {
+    function vote(
+        uint8 _partyId,
+        uint8 _voterId
+    ) public returns (string memory) {
         require(
             msg.sender == voters[_voterId].wallet,
             "You can't vote on others behalf."
         );
         require(!voters[_voterId].voted, "You cannot double vote.");
         require(block.timestamp <= electionEndTime, "Election ended.");
+        require(voters[_voterId].verified, "You are not verified.");
+        require(
+            _partyId < numParties && _partyId > 0,
+            "You can't vote for a party that doesn't exist."
+        );
 
         voters[_voterId].voted = true;
         parties[_partyId].voteCount++;
         return ("You have voted successfully!");
     }
 
-    function endElection() public returns (string memory) {
+    // End election using election id and return election status
+    function endElection() public returns (bool) {
         require(
             msg.sender == owner,
             "You don't have authority to end election."
         );
         require(!electionEnded, "Election already ended.");
-        require(block.timestamp >= electionEndTime, "Election not ended yet.");
-
         electionEnded = true;
-        return ("Election ended successfully!");
+        return electionEnded;
     }
 
+    // Get winner details using election id
     function winnerDetails() public view returns (politicalParty memory) {
         require(electionEnded, "Election not ended yet.");
         uint8 partyId;
@@ -180,6 +223,7 @@ contract election {
         return parties[partyId];
     }
 
+    // Get election details using election id
     function getElectionDetails()
         public
         view
